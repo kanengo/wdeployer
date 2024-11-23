84764e6b07d8b2a51c286c57fb39a8148cc00b5e
kanengo <leekanon@outlook.com>
2024-11-24 02:06:16 +0800

    feat: build脚本

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -7,12 +7,9 @@ else
   echo "上传版本: $1"
 fi
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o kube-mono_linux_amd64 ./cmd/kube-mono
-
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o kube-mono_darwin_amd64 ./cmd/kube-mono
-
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o kube-mono_win_amd64.exe ./cmd/kube-mono
-
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o kube-mono_linux_amd64 ./cmd/kube-mono \
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o kube-mono_darwin_amd64 ./cmd/kube-mono \
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o kube-mono_win_amd64.exe ./cmd/kube-mono \
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o kube-mono_darwin_arm64 ./cmd/kube-mono
 
 user=packageadmin
